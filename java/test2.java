c39d498b89cc0d10fd86cf28c18d17e3950095f9
Tarun Goyal <targoy@qti.qualcomm.com>
2025-05-02 13:25:06 +0530

    Test PR

--- java/test2.java
+++ java/test2.java
@@ -1,4 +1,4 @@
-// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries. 
+// Copyright (c) 2015 Qualcomm Technologies, Inc. and/or its subsidiaries. 
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 and
