bc8107f49a9043cd7a3e0f432d2fc15fcda3847b
Tarun Goyal <tarun4u55@gmail.com>
2025-05-05 20:33:57 +0530

    Update test2.java

--- java/test2.java
+++ java/test2.java
@@ -7,7 +7,6 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
-// SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
